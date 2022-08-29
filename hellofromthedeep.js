c92b81c4876ed7485264fe642caadae79b302b4b
Andrey Krivosheev <fe.ui.ux.be@gmail.com>
2022-08-29 18:10:41 +0200

    feat: add ancient choosing

--- hellofromthedeep.js
+++ hellofromthedeep.js
@@ -9,9 +9,7 @@ const bb1 = document.querySelector('.bb-1');
 const bb2 = document.querySelector('.bb-2');
 const bb3 = document.querySelector('.bb-3');
 
-buttonMiddle.addEventListener('click', fillCountContainer());
-
-function fillCountContainer() {
+function fillCountContainerKtulhu() {
   gb1.innerHTML = 0;
   gb2.innerHTML = 1;
   gb3.innerHTML = 3;
@@ -22,3 +20,50 @@ function fillCountContainer() {
   bb2.innerHTML = 0;
   bb3.innerHTML = 0;
 }
+
+function fillCountContainerShub() {
+  gb1.innerHTML = 1;
+  gb2.innerHTML = 3;
+  gb3.innerHTML = 2;
+  brb1.innerHTML = 2;
+  brb2.innerHTML = 2;
+  brb3.innerHTML = 4;
+  bb1.innerHTML = 1;
+  bb2.innerHTML = 1;
+  bb3.innerHTML = 0;
+}
+
+function fillCountContainerYog() {
+  gb1.innerHTML = 0;
+  gb2.innerHTML = 2;
+  gb3.innerHTML = 3;
+  brb1.innerHTML = 2;
+  brb2.innerHTML = 3;
+  brb3.innerHTML = 4;
+  bb1.innerHTML = 1;
+  bb2.innerHTML = 1;
+  bb3.innerHTML = 0;
+}
+
+function fillCountContainerAzathot() {
+  gb1.innerHTML = 1;
+  gb2.innerHTML = 2;
+  gb3.innerHTML = 2;
+  brb1.innerHTML = 2;
+  brb2.innerHTML = 3;
+  brb3.innerHTML = 4;
+  bb1.innerHTML = 1;
+  bb2.innerHTML = 1;
+  bb3.innerHTML = 0;
+}
+
+const imageAncient = document.querySelector('.image-ancient');
+const imageShubniggurath = document.querySelector('.image-shubniggurath');
+const imageIogsothoth = document.querySelector('.image-iogsothoth');
+const imageAzathoth = document.querySelector('.image-azathoth');
+
+imageAncient.addEventListener('click', fillCountContainerKtulhu());
+imageShubniggurath.addEventListener('click', fillCountContainerShub());
+imageIogsothoth.addEventListener('click', fillCountContainerYog());
+imageAzathoth.addEventListener('click', fillCountContainerAzathot());
+
